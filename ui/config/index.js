64066f5c967155156ce7ecf60c70b02d2d7f7619
Ryan Barnard <rbarnard@arane.us>
2018-04-30 12:50:29 -0400

    UI dev server proxy configuration can be set via environment variable.

--- ui/config/index.js
+++ ui/config/index.js
@@ -1,8 +1,8 @@
-'use strict'
+'use strict';
 // Template version: 1.3.1
 // see http://vuejs-templates.github.io/webpack for documentation.
 
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   dev: {
@@ -11,7 +11,7 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
       proxyTable: {
-          '**': 'http://api:5000'
+          '**': process.env.API_PROXY || 'http://api:5000'
       },
 
     // Various Dev Server settings
@@ -75,4 +75,4 @@ module.exports = {
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
   }
-}
+};
