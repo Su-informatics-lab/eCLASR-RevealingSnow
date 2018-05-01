7ef0847bc9d0988af01aef55560fa631e1f801a0
Ryan Barnard <rbarnard@arane.us>
2018-05-01 18:20:14 -0400

    RS-11 - Added a tooltip to the bar chart.

--- ui/src/components/charts/BarChart.vue
+++ ui/src/components/charts/BarChart.vue
@@ -7,6 +7,8 @@
 
 <script>
     import BarChart from 'britecharts/dist/umd/bar.min';
+    import tooltip from 'britecharts/dist/umd/miniTooltip.min';
+
     import * as d3Selection from 'd3-selection';
 
 
@@ -15,6 +17,7 @@
             return {
                 container: null,
                 chart: null,
+                tooltip: null,
             };
         },
         props: {
@@ -36,16 +39,33 @@
         watch: {
             data(value) {
                 this.container.datum(value).call(this.chart);
+
+                if (!this.tooltip) {
+                    this.createTooltip();
+                }
             },
         },
         mounted() {
             this.container = d3Selection.select(this.$el);
             this.chart = new BarChart();
+
             this.chart
                 .isAnimated(true)
                 .width(this.width)
                 .height(this.height)
                 .margin(this.margin);
+        },
+        methods: {
+            createTooltip() {
+                this.tooltip = tooltip();
+                const tooltipContainer = this.container.select('.bar-chart .metadata-group');
+                tooltipContainer.datum([]).call(this.tooltip);
+
+                this.chart
+                    .on('customMouseOver', this.tooltip.show)
+                    .on('customMouseMove', this.tooltip.update)
+                    .on('customMouseOut', this.tooltip.hide);
+            },
         },
     };
 </script>
