a3008d7d9dec36daf4b3f0befe1353b664fef2ba
Ryan Barnard <rbarnard@arane.us>
2018-05-04 18:06:28 -0400

    RS-19 - Filters can now be either simple values or include a cutoff date. When the value is 0 (exclusion), only patients with condition after cutoff are excluded. When the value is 1 (inclusion), only patients with the condition after the cutoff are excluded.

--- api/snow/filters.py
+++ api/snow/filters.py
@@ -1,10 +1,44 @@
 import pandas as pd
 
+_CRITERION_DATE_CONJUNCTION = {
+    '0': 'or',
+    '1': 'and'
+}
+
+_CRITERION_DATE_COMPARISON = {
+    '0': '<',
+    '1': '>='
+}
+
+
+def _date_field(key):
+    return "{}_date".format(key)
+
+
+def _expand_filter(key, value):
+    if not isinstance(value, dict):
+        return '{} == {}'.format(key, value)
+
+    date_field = _date_field(key)
+    field_value = value['value']
+    date_value = value['date']
+    date_comp = _CRITERION_DATE_COMPARISON[field_value]
+    conjunction = _CRITERION_DATE_CONJUNCTION[field_value]
+
+    return '({field} == {value} {conj} {date_field} {date_comp} "{date_value}")'.format(
+        field=key,
+        value=field_value,
+        conj=conjunction,
+        date_field=date_field,
+        date_comp=date_comp,
+        date_value=date_value
+    )
+
 
 def filter_patients(data: pd.DataFrame, filters: dict) -> pd.DataFrame:
     if filters:
         condition = [
-            "{} == {}".format(key, value)
+            _expand_filter(key, value)
             for key, value in filters.items()
         ]
         data = data.query(' and '.join(condition))
--- api/tests/unit_tests/test_filters.py
+++ api/tests/unit_tests/test_filters.py
@@ -19,19 +19,30 @@ class FilterTests(TestCase):
         }
         self.data = pd.DataFrame(data=data)
 
+    def _get_filtered_patient_nums(self, f):
+        result = filters.filter_patients(self.data, f)
+        return set(result['patient_num'].values)
+
     def test_empty_filter_returns_full_dataset(self):
         result = filters.filter_patients(self.data, dict())
         self.assertTrue(self.data.equals(result))
 
     @parameterized.expand([
-        (0, {1, 2}),
-        (1, {3}),
+        ('0', {1, 2}),
+        ('1', {3}),
     ])
     def test_filter_value_subsets_dataset(self, cardiac_value, expected_patient_nums):
-        result = filters.filter_patients(self.data, {'cardiac': cardiac_value})
-        actual_patient_nums = result['patient_num']
-        self.assertEqual(set(actual_patient_nums.values), expected_patient_nums)
+        actual_patient_nums = self._get_filtered_patient_nums({'cardiac': cardiac_value})
+        self.assertEqual(actual_patient_nums, expected_patient_nums)
 
     def test_multiple_filters_treated_as_and(self):
-        result = filters.filter_patients(self.data, {'cardiac': 1, 'neuro': 1})
-        self.assertEqual(result['patient_num'].values, [3])
+        patient_nums = self._get_filtered_patient_nums({'cardiac': '1', 'neuro': '1'})
+        self.assertEqual(patient_nums, {3})
+
+    def test_exclusion_with_date_excludes_patients_with_condition_after_cutoff(self):
+        patient_nums = self._get_filtered_patient_nums({'neuro': {'value': '0', 'date': '2018-05-01'}})
+        self.assertEqual(patient_nums, {1, 2})
+
+    def test_inclusion_with_date_includes_patients_with_condition_after_cutoff(self):
+        patient_nums = self._get_filtered_patient_nums({'neuro': {'value': '1', 'date': '2018-05-01'}})
+        self.assertEqual(patient_nums, {3})
