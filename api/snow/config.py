6f7292ac1d61f395845926773ff28e65ffa472d0
Ryan Barnard <rbarnard@arane.us>
2018-10-02 17:14:45 -0400

    RS-84 - Environment variables can now also be loaded from a file on disk without needing to be part of the API's environment at launch. This will facilitate providing secrets to the API even in the case of running in Docker.

--- api/snow/config.py
+++ api/snow/config.py
@@ -1,4 +1,27 @@
+import logging
+import os
+
 from flask_env import MetaFlaskEnv
+
+from snow import constants as C
+
+logger = logging.getLogger(__name__)
+
+
+def load_environment_file(filename):
+    # Load the contents of the file (key=value) to the environment
+    if os.path.exists(filename):
+        logger.debug("Importing environment from %s", filename)
+
+        for line in open(filename):
+            line = line.strip()
+            if not line.startswith('#'):
+                key, value = line.split('=')
+                os.environ[key] = value
+
+
+# Load any external environment variables before the configuration class definition
+load_environment_file(os.environ.get(C.RSENV_FILE) or C.DEFAULT_ENVIRONMENT_FILE)
 
 
 class Configuration(metaclass=MetaFlaskEnv):
