d471cffe00f6691bfe764525723026c3a189778f
Ryan Barnard <rbarnard@arane.us>
2018-12-21 16:25:29 -0500

    RS-147 - Changed the name and target of the entry point script.

--- api/setup.py
+++ api/setup.py
@@ -39,7 +39,7 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'start = snow.app:main',
+            'Start Visualization = snow.app:runtornado',
         ],
     },
     install_requires=[
