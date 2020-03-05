069caf47cc42b56b0a2acc6db3c09e7bd34b781f
Ryan Barnard <rbarnard@arane.us>
2020-03-05 16:55:24 -0500

    RS-176 - YMCA sites are now included when loading the overall patient stats, both filtered and unfiltered.

--- ui/src/store/actions.js
+++ ui/src/store/actions.js
@@ -8,30 +8,35 @@ import * as types from './mutation-types';
 export default {
     setActiveFilters({ commit, dispatch }, { criteria, sites }) {
         commit(types.SET_ACTIVE_FILTERS, { criteria, sites });
-        dispatch('getFilteredStats', criteria);
+        dispatch('getFilteredStats', { criteria, sites });
     },
-    setActiveSites({ commit }, { sites }) {
+    setActiveSites({ commit, dispatch }, { sites }) {
         commit(types.SET_ACTIVE_YMCA_SITES, { sites });
+
+        dispatch('reloadTotals');
     },
-    setResultLimits({ commit, dispatch, state }, { limits }) {
+    setResultLimits({ commit, dispatch }, { limits }) {
         // eslint-disable-next-line no-underscore-dangle
         this._vm.$api.setResultLimits(limits);
         commit(types.SET_RESULT_LIMITS, { limits });
 
+        dispatch('reloadTotals');
+    },
+    reloadTotals({ dispatch, state }) {
         // Need to reload everything
         dispatch('loadUnfilteredStats');
-        dispatch('getFilteredStats', state.filters.criteria);
+        dispatch('getFilteredStats', state.filters);
     },
-    loadUnfilteredStats({ commit }) {
+    loadUnfilteredStats({ commit, state }) {
         // eslint-disable-next-line no-underscore-dangle
-        this._vm.$api.getPatientStats().then((result) => {
+        this._vm.$api.getPatientStats({}, state.filters.ymcaSites).then((result) => {
             const stats = _.mapValues(result, objectToArray);
             commit(types.LOAD_UNFILTERED_STATS, stats);
         });
     },
-    getFilteredStats({ commit }, criteria) {
+    getFilteredStats({ commit }, { criteria, ymcaSites }) {
         // eslint-disable-next-line no-underscore-dangle
-        this._vm.$api.getPatientStats(criteria).then((result) => {
+        this._vm.$api.getPatientStats(criteria, ymcaSites).then((result) => {
             const stats = _.mapValues(result, objectToArray);
             commit(types.LOAD_FILTERED_STATS, stats);
         });
